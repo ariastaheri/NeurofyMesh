d8f2f57f3df352f4000cc230745a78d3161e0df5
ariastaheri <aria.staheri@gmail.com>
2023-07-12 17:23:57 -0400

    update services

--- NeurofyMesh/Services/IVendorUserService.cs
+++ NeurofyMesh/Services/IVendorUserService.cs
@@ -7,10 +7,9 @@ namespace NeurofyMesh.Services
         List<VendorUser> GetVendorUsers();
         VendorUser? GetVendorUserById(int vendorUserId);
         VendorUser? GetVendorUserByUserId(int userId);
-        VendorUser? GetVendorUserByVendorId(int vendorId);
+        List<VendorUser>? GetVendorUsersByVendorId(int vendorId);
         Task<bool> UpdateVendorUser(int id, VendorUser vendorUser);
         Task<bool> DeleteVendorUser(int vendorUserId);
-
         Task<VendorUser> CreateVendorUser(VendorUser vendorUser);
     }
 }
